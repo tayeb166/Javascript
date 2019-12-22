ceac7699877a442dcc6f70d76c7dc6274d8172bb
tayeb166 <58751925+tayeb166@users.noreply.github.com>
2019-12-22 15:25:28 -0500

    1

---
+++ Dog_years/app.js
@@ -0,0 +1,19 @@
+//my current age
+const myAge = 24;
+//early years
+let earlyYears = 2;
+earlyYear *= 10.5;
+let laterYears = myAge -2;
+laterYear *= 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+
+let myAgeInDogYears = earlyYears + laterYears;
+
+let myName = "Tayeb".toLocaleLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
+
+
--- kelvin_weather/app.js
+++ kelvin_weather/app.js
@@ -7,3 +7,8 @@ let fahrenheit = celsius * (9/5) + 32;
 //ROund down fahreint variable
 fahrenheit = Math.floor(fahrenheit);
 console.log(`The temprature ${fahrenheit} degrees Fahrenheit.`)
+
+let newton = celsius * (33/100);
+//ROund down fahreint variable
+newton = Math.floor(newton);
+console.log(`The temprature in ${newton} degrees Newton.`)
